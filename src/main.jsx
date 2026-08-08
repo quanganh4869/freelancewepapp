c66719cec5840b0af75c72c52b2b175583bdad4a
anhnq-cmd <anh.nq@picontechnology.com>
2026-08-08 14:09:04 +0700

    feat: implement modal contact form with detailed ui, fix git tracking for system files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { Agentation } from 'agentation'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {import.meta.env.DEV && <Agentation />}
   </React.StrictMode>,
 )
